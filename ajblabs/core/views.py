9bd0108fd7e3c55cae2a739dbbe993908f1da299
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-09-29 23:28:26 -0400

    junk

--- ajblabs/core/views.py
+++ ajblabs/core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def home(request):
     return render(request, 'core/index.html', {})
+
+def about(request):
+    return render(request, 'core/about.html', {})
+
+def contact(request):
+    return render(request, 'core/contact.html', {})
