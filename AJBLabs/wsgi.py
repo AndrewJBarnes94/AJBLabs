4790bc0a46d72c10b376e6abdc126479bf211ec3
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-09-30 21:29:19 -0400

    Base.html & Home.html

--- AJBLabs/wsgi.py
+++ AJBLabs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ajblabs project.
+WSGI config for AJBLabs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ajblabs.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AJBLabs.settings')
 
 application = get_wsgi_application()
--- ajblabs/blog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def blog(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/blog.html', {'posts': posts})
