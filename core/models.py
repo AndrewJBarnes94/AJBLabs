d991ff724a15d58b9b1f92056a1426d1768574c0
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-10-09 23:36:35 -0400

    trying out aws s3 bucket

--- core/models.py
+++ core/models.py
@@ -18,7 +18,7 @@ class ProjectField(models.Model):
 class Project(models.Model):
     field = models.ForeignKey(ProjectField, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    code_zip = models.FileField(upload_to='portfolio_code_zipped/')
+    code_zip = models.FileField(upload_to='media/portfolio_code_zipped/')
 
     def __str__(self):
         return self.title
