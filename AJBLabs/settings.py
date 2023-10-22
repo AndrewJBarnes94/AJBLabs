569ff53df586e6e9fde4890b103825a8bd9a9e67
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-10-21 22:22:22 -0400

    comment out email vars in settings

--- AJBLabs/settings.py
+++ AJBLabs/settings.py
@@ -143,10 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 WHITENOISE_USE_FINDERS = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+'''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'andrewjbarnes94@gmail.com'  # Your Gmail address.
 EMAIL_HOST_PASSWORD = 'Qwertyuiop1313'     # Your Gmail password or App Password.
+'''
